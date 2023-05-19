394c8721c60746b9fc04bae840dd28fc91f905ac
aycrown1 <ayoadeadedamola198@gmail>
2023-05-19 23:05:19 +0100

    implementation of the singly link list functions as helpers in the program

--- link_list2.c
+++ link_list2.c
@@ -5,6 +5,7 @@ size_t print_strnode(const list_t *);
 int remove_index(list_t **, unsigned int);
 void free_list(list_t **);
 
+
 /**
  * append_node - adds a node to the end of the list
  * @head: address of pointer to head node
@@ -14,35 +15,35 @@ void free_list(list_t **);
  */
 list_t *append_node(list_t **head, const char *string, int num)
 {
-list_t *new_node, *node;
+	list_t *new_node, *node;
 
-if (!head)
-return (NULL);
+	if (!head)
+		return (NULL);
 
-node = *head;
-new_node = malloc(sizeof(list_t));
-if (!new_node)
-return (NULL);
-memset((void *)new_node, 0, sizeof(list_t));
-new_node->num = num;
-if (string)
-{
-new_node->string = _strdup(string);
-if (!new_node->string)
-{
-free(new_node);
-return (NULL);
-}
-}
-if (node)
-{
-while (node->next)
-node = node->next;
-node->next = new_node;
-}
-else
-*head = new_node;
-return (new_node);
+	node = *head;
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+	memset((void *)new_node, 0, sizeof(list_t));
+	new_node->num = num;
+	if (string)
+	{
+		new_node->string = _strdup(string);
+		if (!new_node->string)
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
 }
 
 /**
@@ -52,16 +53,16 @@ return (new_node);
  */
 size_t print_strnode(const list_t *h)
 {
-size_t i = 0;
+	size_t i = 0;
 
-while (h)
-{
-_puts(h->string ? h->string : "(nil)");
-_puts("\n");
-h = h->next;
-i++;
-}
-return (i);
+	while (h)
+	{
+		_puts(h->string ? h->string : "(nil)");
+		_puts("\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
 
 /**
@@ -72,35 +73,35 @@ return (i);
  */
 int remove_index(list_t **head, unsigned int index)
 {
-list_t *current, *previous;
-unsigned int i = 0;
+	list_t *current, *previous;
+	unsigned int i = 0;
 
-if (!head || !*head)
-return (0);
+	if (!head || !*head)
+		return (0);
 
-if (!index)
-{
-current = *head;
-*head = (*head)->next;
-free(current->string);
-free(current);
-return (1);
-}
-current = *head;
-while (current)
-{
-if (i == index)
-{
-previous->next = current->next;
-free(current->string);
-free(current);
-return (1);
-}
-i++;
-previous = current;
-current = current->next;
-}
-return (0);
+	if (!index)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current->string);
+		free(current);
+		return (1);
+	}
+	current = *head;
+	while (current)
+	{
+		if (i == index)
+		{
+			previous->next = current->next;
+			free(current->string);
+			free(current);
+			return (1);
+		}
+		i++;
+		previous = current;
+		current = current->next;
+	}
+	return (0);
 }
 
 /**
@@ -110,18 +111,18 @@ return (0);
  */
 void free_list(list_t **head_ptr)
 {
-list_t *node, *next_node, *head;
+	list_t *node, *next_node, *head;
 
-if (!head_ptr || !*head_ptr)
-return;
-head = *head_ptr;
-node = head;
-while (node)
-{
-next_node = node->next;
-free(node->string);
-free(node);
-node = next_node;
-}
-*head_ptr = NULL;
+	if (!head_ptr || !*head_ptr)
+		return;
+	head = *head_ptr;
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node->string);
+		free(node);
+		node = next_node;
+	}
+	*head_ptr = NULL;
 }
